419992a2ddfae31c84fee8a818549e2afdaa2b76
VaacSS <santiago.vargas6739@alumnos.udg.mx>
2023-05-04 17:03:41 -0600

    a

---
+++ p5.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n,d[30],d1=0,r=1;
+    int a[15]={2,3,5,7,11,13,17,19,23,29,31,37,41,43,47};
+    cin>>n;
+    int n1[n]; 
+    for(int i = 1; i<=n;i++){
+        cin>>n1[i-1];
+    }
+    for(int i = 1; i<=n;i++){
+       for(int j = 0; j<15;j++){
+            alv:
+            if(n1[i-1]%a[j]==0){
+                d[d1]=a[j];
+                d1++;
+                n1[i-1]/=a[j];
+                for(int k=0;k<=n;k++){
+                    if(n1[k]%a[j]==0){
+                        n1[k]/=a[j];
+                    }
+                }
+                if(n1[i-1%a[j]==0]){
+                    goto alv;
+                }
+            }
+        } 
+    }
+    for(int i=0; i<d1;i++){
+        r*=d[i];
+    }
+    cout<<r;
+}
