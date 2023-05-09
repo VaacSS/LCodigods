093f5042897931d72a4b8eb719e78c08321d3fe7
VaacSS <santiago.vargas6739@alumnos.udg.mx>
2023-05-09 11:08:27 -0600

    a

--- pruebas.cpp
+++ pruebas.cpp
@@ -1,34 +1,11 @@
 #include <iostream>
 using namespace std;
-int main() {
-    int a = 5, b = 7;
-    int sum = a + b;
-    cout << "La suma de " << a << " y " << b << " es " << sum << endl;
-    return 0;
+
+class suma{
+    public:
+    int a;
+    private:
+    int b;j
 }
-/*
-Copy code
-#include <iostream>
-
-using namespace std;
-
-class Suma {
-public:
-    Suma(int a, int b) {
-        this->a = a;
-        this->b = b;
-    }
-    int calcular() {
-        return a + b;
-    }
-private:
-    int a, b;
-};
-
 int main() {
-    int a = 5, b = 7;
-    Suma s(a, b);
-    int sum = s.calcular();
-    cout << "La suma de " << a << " y " << b << " es " << sum << endl;
-    return 0;
-}*/
+}
